24f7c63d9a284339ddd32ce5b8853739d13396bc
netcse <smamran09@gmail.com>
2015-09-06 11:49:04 +0600

    dummy message

--- Csharp/ImageDownloadFromNetwork.cs
+++ Csharp/ImageDownloadFromNetwork.cs
@@ -11,11 +11,11 @@ using System.Windows.Forms;
 public class ImageDownloadFromNetwork{
 	
 	public static void Main(String[] args){
-		string picUri = "http://apache-netcse.c9.io/restaurants/kfc/Shredded-Chicken-and-Avocado.jpg";
+		string picUri = "http://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
 		WebRequest requestPic = WebRequest.Create(picUri);
 		WebResponse responsePic = requestPic.GetResponse();
 		Image webImage = Image.FromStream(responsePic.GetResponseStream());
-		webImage.Save("food.jpg");
+		webImage.Save("google.jpg");
 	}
 	
 }
